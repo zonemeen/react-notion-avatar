dca9d27bc511f26c004ac8515ed3a3d564eb3205
zonemeen <miqilin18@gmail.com>
2022-03-07 19:09:22 +0800

    fix: import path error

--- example/src/App/index.tsx
+++ example/src/App/index.tsx
@@ -3,8 +3,8 @@ import { hot } from 'react-hot-loader'
 import domtoimage from 'dom-to-image'
 import { saveAs } from 'file-saver'
 
-import NotionAvatar, { getRandomConfig } from '../../../dist/index.mjs'
-import type { AvatarConfig, AvatarPart, ShapeTypes } from '../../../dist'
+import NotionAvatar, { getRandomConfig } from 'react-notion-avatar'
+import type { AvatarConfig, AvatarPart, ShapeTypes } from 'react-notion-avatar'
 
 import AvatarEditor from './AvatarEditor/index'
 import Footer from './Footer/index'
