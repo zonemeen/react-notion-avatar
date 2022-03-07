2080256edf0f2606e1a4120151873a5f51d2d537
zonemeen <miqilin18@gmail.com>
2022-03-07 17:17:59 +0800

    refactor: modify the bottom description

--- example/src/App/Footer/index.tsx
+++ example/src/App/Footer/index.tsx
@@ -4,21 +4,14 @@ export default function footer() {
   return (
     <footer className="flex items-center justify-center text-gray-400 text-sm pb-5">
       <a
-        href="https://github.com/dapilab/react-nice-avatar"
+        href="https://github.com/zonemeen/react-notion-avatar"
         className="transition-opacity duration-300 hover:opacity-75"
       >
         Github
       </a>
       <span className="mx-3 relative -top-1">.</span>
       <a
-        href="https://twitter.com/dapi_labs"
-        className="transition-opacity duration-300 hover:opacity-75"
-      >
-        Twitter
-      </a>
-      <span className="mx-3 relative -top-1">.</span>
-      <a
-        href="https://www.buymeacoffee.com/wwayne"
+        href="https://www.buymeacoffee.com/miqilin18P"
         className="transition-opacity duration-300 hover:opacity-75"
       >
         Buy Me A Coffee
