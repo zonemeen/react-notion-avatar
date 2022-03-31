a71fdcc9b4b66421e3e48281cb9e61d2655ffbc7
zonemeen <miqilin18@gmail.com>
2022-03-31 14:21:23 +0800

    refactor: ts type

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,37 +2,22 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import { Translations } from './translations'
 
+interface TranslationMap {
+  [key: string]: string
+}
+
 type TranslateType = {
   id: string
   name: string
-  translation: {
-    'tip.Face': string
-    'tip.Eye': string
-    'tip.Eyebrow': string
-    'tip.Glass': string
-    'tip.Hair': string
-    'tip.Mouth': string
-    'tip.Nose': string
-    'tip.Beard': string
-    'tip.Accessory': string
-    'tip.Detail': string
-    'tip.Shape': string
-    'tip.Flip': string
-    'tip.Palette': string
-    'tip.Random': string
-    'tip.Config': string
-    'tip.Download': string
-  }
+  translation: TranslationMap
 }
 
-export type LangType = 'en' | 'zh'
-
-type Resource = {
-  [id in LangType]: TranslateType
+interface Resource {
+  [id: string]: TranslateType
 }
 
 const resources = Translations.reduce((obj, curr) => {
-  obj[curr.id as LangType] = curr
+  obj[curr.id] = curr
   return obj
 }, {} as Resource)
 
